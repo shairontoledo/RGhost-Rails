696de9eb1c6cda138b7f96183019e8da040e2b8a
Felipe Diesel <felipediesel@gmail.com>
2010-03-16 20:32:10 +0800

    If view_path had more than one item, not finding right view.

--- lib/rghost_rails.rb
+++ lib/rghost_rails.rb
@@ -8,13 +8,13 @@ class ActionController::Base
   def rghost_render(format, options)
     v=case options[:report]
     when Hash
-      File.join(view_paths, options[:report][:controller] ||self.controller_name, options[:report][:action])
+      File.join(view_paths.first, options[:report][:controller] ||self.controller_name, options[:report][:action])
     when String,Symbol
-      File.join(view_paths, controller_path, options[:report].to_s)
+      File.join(view_paths.first, controller_path, options[:report].to_s)
     when NilClass
-      File.join(view_paths, controller_path, self.action_name)
+      File.join(view_paths.first, controller_path, self.action_name)
     end
- 
+
     ActionView::Helpers.included_modules.each{|m| extend m}
     r=File.readlines("#{v}.rghost.rb")
     @__rgdoc__=nil
@@ -23,12 +23,12 @@ class ActionController::Base
     options.delete(:report)
     filename=options.delete(:filename)
     disposition=options.delete(:disposition)
- 
+
     rg=@__rgdoc__.render(format,options)
     out=rg.output
     raise "RGhost::Error #{rg.errors} - #{out}" if rg.error?
     data=out.readlines.join
     rg.clear_output
     send_data(data, :filename => filename, :disposition => disposition, :type => Mime::Type.lookup_by_extension(format.to_s))
-  end  
+  end
 end
